3e1c8dd83de2cc36ade9e717f4fa5086d37b6a2f
reuschj <reuschj@gmail.com>
2022-12-29 00:36:57 -0600

    Initial Commit

---
+++ rockpaperscissors/rockpaperscissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  rockpaperscissorsApp.swift
+//  rockpaperscissors
+//
+//  Created by Justin Reusch on 12/29/22.
+//
+
+import SwiftUI
+
+@main
+struct rockpaperscissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
